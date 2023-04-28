4a2d58b7d0052c15ee2e2e57d894c168a640b254
adam0913 <andykorea@gmail.com>
2023-04-28 13:29:18 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,33 @@
-// var express = require('express');
-// var router = express.Router();
+var express = require('express');
+var router = express.Router();
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index.html');
-// });
-
-// module.exports = router;
-
-
-const express = require('express');
-var cors = require('cors')
-const app = express();
-const port = 3000;
-
-app.use(cors())
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index.html');
 });
-app.get('/sound/:name', (req, res) => {
-	const {name} = req.params
-	console.log(name)
-	if(name=='dog'){
-		res.json({'sound':'멍멍'})
-	}else if(name == 'cat'){
-		res.json({'sound':'야옹'})
-	}else{
-		res.send('알수없슴')
-	}
-	})
 
-app.listen(port, () => {})
+module.exports = router;
+
+
+// const express = require('express');
+// var cors = require('cors')
+// const app = express();
+// const port = 3000;
+
+// app.use(cors())
+// app.get('/', (req, res) => {
+//     res.send('Hello World!');
+// });
+// app.get('/sound/:name', (req, res) => {
+// 	const {name} = req.params
+// 	console.log(name)
+// 	if(name=='dog'){
+// 		res.json({'sound':'멍멍'})
+// 	}else if(name == 'cat'){
+// 		res.json({'sound':'야옹'})
+// 	}else{
+// 		res.send('알수없슴')
+// 	}
+// 	})
+
+// app.listen(port, () => {})
